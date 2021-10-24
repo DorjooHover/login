7e41bd6c756219e72c58095aa0ebc03705191416
fuckingusername90 <60386060+fuckingusername90@users.noreply.github.com>
2021-10-24 19:35:35 +0800

    secret changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const { pool } = require('./dbConfig');
 const bcrypt = require('bcrypt')
-const session = require('cookie-session')
+const session = require('express-session')
 const flash = require('express-flash')
 const passport = require('passport')
 
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(session({
     adapter: "connect-pg-simple",
-    secret: 'secret',
+    secret: process.env.SESSION_SECRET = 'secret',
     saveUninitialized: false,
     resave: false,
     unset: 'destroy',
